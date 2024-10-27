8db9982c476d30ca578716e73f267ff44cb0996c
ricardomartinsuy <77988270+ricardomartinsuy@users.noreply.github.com>
2024-10-27 14:15:01 -0300

    created home and game components

--- GameOfDronesFront/src/app/app-routing.module.ts
+++ GameOfDronesFront/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'; 
+import { GameComponent } from './components/game/game.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'game', component: GameComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redireciona para home para qualquer rota desconhecida
 ];
 
 @NgModule({
--- GameOfDronesFront/src/app/app.modules.ts
+++ GameOfDronesFront/src/app/app.modules.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module'; 
-import { AppComponent } from './app.component'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
